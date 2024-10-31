f1315b83321665b15c078f3bf5228f53447d5154
Lbarrerob <l.barrerob@uniandes.edu.co>
2024-10-30 23:33:39 -0500

    primero

---
+++ serie.ts
@@ -0,0 +1,7 @@
+export class Serie {
+    constructor(public id:number, public nombre:string, public franquisea:string, public temporadas:number, 
+        public descripcion:string, public enlace:string, public imagen:string)
+    {
+
+    }
+}
